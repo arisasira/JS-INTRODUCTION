13033916aa02e277a6b6e1f632c1169863ac0457
arisasira <59421917+arisasira@users.noreply.github.com>
2020-01-01 19:46:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ X *= 7; //X=X * 7, 5*7 , output: 35
 console.log(X); //Creating variables and manipulating values
 
 let myName = ('Arisa');
-const myCity = ('Tempe');
-const myAge = 23;
+const myCity = ('Washington');
+const myAge = 42;
 console.log(`My name is ${myName}. I live in ${myCity}. My age is ${myAge}.`); //Interpolate multiple variables into a string
 
 console.log('My name is ' + myName +' and I live in '+ myCity +'. I am currently '+ myAge +'.'); //Concatenating strings using variables of different data types
